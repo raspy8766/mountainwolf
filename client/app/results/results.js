cd4166bf7748690a1df87e951a9c1af2eb9bbb0d
Sean Conroy <conroysean@gmail.com>
2015-04-14 15:06:18 -0700

    [refactor] test branch refactor

---
+++ client/app/results/results.js
@@ -0,0 +1,25 @@
+  angular.module('infoodity.searchResults', [])
+
+    .controller('ResultsController',['$scope', 'Results', '$location', function ($scope, Results, $location){
+
+      var nameSearched = $location.path().split('=').pop();
+
+      $scope.data = {} // getData
+
+      $scope.getResults = function (name) { // function used to populate data field (is parameter data necessary??)
+        console.log(name);
+        Results.getResults(name) // call $http request
+          .then(function (data) {
+            console.log(data);
+            $scope.results = data;
+            console.log(data); // on return of $http call, populate data
+          })
+          .catch(function (error) {
+            console.error(error); // if error on return of $http call, return error
+          });
+      };
+
+      $scope.getResults(nameSearched);
+
+  // reroute to restaurant page
+    }]);
--- server/api/apiController.js
+++ server/api/apiController.js
@@ -2,16 +2,16 @@ var data = require('../data/testData');
 
 module.exports = {
   getRestaurantsWithName: function(req, res, next) {
-    
+
     var restaurants = data.data.restaurants;
     var list = [];
     restaurants.forEach(function(restaurant){
       if (restaurant.name === req.query.name) {
         list.push(restaurant);
       }
-      
+
     })
-    res.end(JSON.stringify(list[0]));
+    res.end(JSON.stringify(list));
   },
 
   getRestaurantWithID: function(req, res, next) {
@@ -31,11 +31,13 @@ module.exports = {
     var reviews = data.data.reviews;
 
     var list = [];
-    var id = Number(req.params.ID);
+    var id = Number(req.params.id);
+    console.log(req.url, id)
     reviews.forEach(function(review) {
       if (review.restaurantID === id) {
         list.push(review);
       }
+      console.log(list);
     })
     res.end(JSON.stringify(list));
   },
