13e9a3a4623eb12f32892c8fc698ecfd7099b7a7
Ross Rasmussen <raspy8766@gmail.com>
2015-04-15 19:05:43 -0700

    [fix] basic layout of the routes

--- client/app/app.js
+++ client/app/app.js
@@ -1,63 +1,52 @@
-var app = angular.module('infoodity', [
-  'infoodity.restaurantPage',
-  'infoodity.searchResults',
-  'infoodity.services',
-  'infoodity.reviews',
-  'infoodity.submitReview',
-  'ui.router'
-  ])
 
-  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-<<<<<<< HEAD
-      .state('restaurantPage', {
-        url: '/restaurantpage',
-        templateUrl: './app/client/restaurantPage/restaurantPage.html',
-        controller: 'RestaurantController',
-        views: {
-        "main-content": { templateUrl: "./app/client/restaurantPage/restaurantPage.html" },
-        "secondary-content": { templateUrl: "./app/client/reviews/reviews.html" }
+var infoodity = angular.module('infoodity', ['ui.router'])
+  
+
+infoodity.config(function($stateProvider, $urlRouterProvider) {
+    
+  $urlRouterProvider.otherwise('home');
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      views: {
+        'viewA': { 
+          template: '<h1> Welcome! Please Choose a Restaurant Above </h1>' 
         }
-      })
-    $stateProvider
-      .state('results', {
-        url: '/results',
-        template: './app/client/results/results.html',
-        controller: 'ResultsController'
-      })
-    $stateProvider
-      .state('submitReview', {
-        url: '/submitReview',
-        templateUrl: './app/client/submitReview/submitReview.html',
-        controller: 'SubmitReviewController'
-      })
-=======
-
-      .state('restaurantPage', {
-        url: '/restaurantpage',
-        templateUrl: './app/restaurantPage/restaurantPage.html',
-        controller: 'RestaurantController',
-        views: {
-        "main-content": { templateUrl: "./app/restaurantPage/restaurantPage.html" },
-        "secondary-content": { templateUrl: "./app/reviews/reviews.html" }
+      }
+    })
+    .state('searchResults', {
+      url: '/search-results',
+      views: {
+        'viewA': { 
+          templateUrl: 'searchResults/results_lance.html',
+  //      controller: 'results/results.js'
         }
-      })
-
-      .state('restaurantPage.submitReview', {
-        url: '/submitReview',
-        templateUrl: './app/submitReview/submitReview.html',
-        controller: 'SubmitReviewController'
-      })
-
-      .state('results', {
-        url: '/results',
-        templateUrl: './app/results/results.html',
-        controller: 'ResultsController',
-        views: {
-        "main-content": { templateUrl: "./app/results/results.html" }
+      }
+    })
+    .state('restaurantPage', {
+      url: '/restaurant-page',
+      'views': {
+        'viewA': {
+          templateUrl: 'restaurantPage/restaurantPage.html',
+    //    controller: 'restaurantPage/restaurantPage.js'
         }
-      })
-
->>>>>>> cf8b200f75711852019662ee61afaa061a40eae9
-  }]);
+      }
+    })
+    .state('reviews', {
+      parent: 'restaurantPage',
+      url: '/reviews',
+      views: {
+        'viewB': { 
+          template: 'reviews/reviews.html',
+  //      controller: 'reviews/reviews.js'
+        }
+      }
+    })
+//     .state('post', {
+//      url: '/results',
+//      parent: 'restaurantPage',
+//      templateUrl: 'results/results.html',
+//      controller: 'results/results.js'
+//    })
+});
